ade03e8b9ca8b7b7bace43b0421c21539392ad99
mateusz-rozek <mateusz.rozek11@vp.pl>
2024-10-15 14:55:16 +0200

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+
+
+int main()
+{
+	double 
+}
+
+
+/*int main()
+{	
+	int o;
+	double e;
+	double sum = 0;
+	int l = 0;
+	double min=0;
+	double max=0;
+	do
+	{
+		std::cout << "podaj liczbe ";
+		std::cin >> e;
+		sum = sum + e;
+		l++; 
+		if (l == 1) {
+			min = e;
+			max = e;
+		}
+		else {
+			if (e < min)
+			{
+				min = e;
+			}
+			if (e > max)
+			{
+				max = e;
+			}
+		}
+		std::cout << "czy chcesz kontynuować? tak-1 nie-2 ";
+		std::cin >> o;
+	} while (o != 2);
+	std::cout << "srednia to " << sum / l << std::endl;
+	std::cout << "minium to " << min << std::endl;
+	std::cout << "maksimum to " << max << std::endl;
+}*/
+
+
+
+
+/*int main()
+{
+	int i;
+	int n;
+	std::cout << "podaj ilosc liczb ";
+	std::cin >> n;
+	std::cout << "pierwsze 10 liczb naturalnych ";
+		while(n > 0)		{
+			std::cout << n << " ";
+			n--;
+		}
+
+}*/
+
+
+/*int main()
+{
+	int i;
+	int n;
+	std::cout << "podaj ilosc liczb ";
+	std::cin >> n;
+	std::cout << "pierwsze 10 liczb naturalnych to ";
+	for (int i = 0; i <= n; i++)
+		std::cout << i << " ";
+}*/

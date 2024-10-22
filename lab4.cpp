df6a6e9c85488aae1bc41f6ba509928a7dc40cd4
mateusz-rozek <mateusz.rozek11@vp.pl>
2024-10-22 14:59:11 +0200

    Add files via upload

---
+++ lab4.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <random>
+using namespace std;
+
+int main() {
+	const int n = 10;
+	int tab[10][10];
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			tab[i][j] = (i + 1)*(j + 1);
+			cout << tab[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
+/*int main() {
+	double pod;
+	const int n = 5;
+	double tab[n];
+	for (int i = 0; i < n; i++) {
+		tab[i] = double(rand()) / RAND_MAX;
+		cout << tab[i] << endl;
+	}
+	for (int i = 0; i < n/2; i++) {
+		pod = tab[n - 1 - i];
+		tab[n - 1 - i] = tab[i];
+		tab[i] = pod;
+	}
+	for (int i = 0; i < n; i++) {
+		cout << tab[i] << endl;
+	}
+	return 0;
+}*/
+
+/*int main() {
+	const int n = 5;
+	double tab[n];
+	double min;
+	for (int i = 0; i < n; i++) {
+		tab[i] = double(rand()) / RAND_MAX;
+		cout << tab[i] << endl;
+		if (i == 0){
+			min = tab[i];
+		}
+		else{
+			if (tab[i] < min){
+				min = tab[i];
+			}
+
+		}
+
+	}
+	cout << "najmniejsza liczba to " << min;
+	return 0;
+}*/
+
+/*int main() {
+	const int n = 5;
+	double tab[n];
+	for (int i = 0; i < n; i++) {
+		tab[i] = double(rand()) / RAND_MAX;
+		cout << tab[i] << endl;
+	}
+	return 0;
+}*/
